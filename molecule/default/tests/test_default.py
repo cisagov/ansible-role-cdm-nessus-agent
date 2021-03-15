7da1938aa583d3a2360eebc941571fe4158ed432
Jeremy Frasier <jeremy.frasier@trio.dhs.gov>
2021-03-15 12:28:13 -0400

    Add commands to link the agent to the Nessus server

--- molecule/default/tests/test_default.py
+++ molecule/default/tests/test_default.py
@@ -4,6 +4,7 @@
 import os
 
 # Third-Party Libraries
+import pytest
 import testinfra.utils.ansible_runner
 
 testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
@@ -24,3 +25,19 @@ def test_nessus_agent_installed(host):
 def test_nessus_agent_enabled(host):
     """Test that Nessus Agent is enabled."""
     assert host.service("nessusagent").is_enabled
+
+
+@pytest.mark.parametrize(
+    "key, value",
+    [
+        ("groups", "good,bad,ugly"),
+        ("key", "0123456789abcdef0123456789abcdef0123456789abcdef0123456789abcdef"),
+        ("ms_server_ip", "nessus.example.com"),
+        ("ms_server_port", "8080"),
+    ],
+)
+def test_nessus_config(host, key, value):
+    """Test that Nessus Agent is configured."""
+    assert value in host.check_output(
+        f"/opt/nessus_agent/sbin/nessuscli fix --secure --get {key}"
+    )
